37c18f797d26f2ac91294b45ce500895efe82760
simonsayswhaat <simonmill@gmail.com>
2016-08-24 00:37:13 +0200

    Added Images / worked on functions to change link
    Research why document.getElementsByClassName won't work.

--- js/quotations_list.js
+++ js/quotations_list.js
@@ -1,101 +1,124 @@
 var quotations = [
 { 
 	quote: "One way to forget about pain is to do something you will be in, completely. So. computer games.",
-	by: "Danil 'Dendi' Ishutin",
-	category: "gaming"
+	source: "Danil 'Dendi' Ishutin",
+	category: "gaming",
+	image: "images/dendi.jpg",
+	citation: "Free to Play Movie",
+	year: "1991"
 },
 {
 	quote: "S T F U",
-	by: "Clinton 'Fear' Loomis",
-	category: "gaming"
+	source: "Clinton 'Fear' Loomis",
+	category: "gaming",
+	image: "images/fear.jpg"
 },
 {
 	quote: "gg China EZ",
-	by: "Syed Sumail 'SumaiL' Hassan",
-	category: "gaming"
+	source: "Syed Sumail 'SumaiL' Hassan",
+	category: "gaming",
+	image: "images/sumail.jpg"
 },
 {
 	quote: "Video Games don't make people violent - LAG DOES!",
-	by: "Unknown",
-	category: "gaming"
+	source: "Unknown",
+	category: "gaming",
+	image: "images/unknown_1.jpg"
 },
 {
 	quote: "I don't need to 'Get A Life.' I'm a gamer. I have lots of lives!",
-	by: "Unknown",
-	category: "gaming"
+	source: "Unknown",
+	category: "gaming",
+	image: "images/unknown_2.jpg"
 },
 {
 	quote: "When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.",
-	by: "Bob Monkhouse",
-	category: "comedy"
+	source: "Bob Monkhouse",
+	category: "comedy",
+	image: "images/bob_monkhouse.jpg"
 },
 {
 	quote: "Always borrow money from a pessimist. He won’t expect it back.",
-	by: "Oscar Wilde",
-	category: "comedy"
+	source: "Oscar Wilde",
+	category: "comedy",
+	image: "images/oscar_wilde.jpg"
 },
 {
 	quote: "I saw the movie, 'Crouching Tiger, Hidden Dragon' and was surprised because I didn't see any tigers or dragons. And then I realized why: they're crouching and hidden.",
-	by: "Steve Martin",
-	category: "comedy"
+	source: "Steve Martin",
+	category: "comedy",
+	image: "images/steve_martin.jpg"
 },
 {
 	quote: "Evening news is where they begin with “Good evening,” and then proceed to tell you why it isn’t.",
-	by: "Anonymous",
-	category: "comedy"
+	source: "Anonymous",
+	category: "comedy",
+	image: "images/anonymous.jpg"
 },
 {
 	quote: "Instead of getting married again, I’m going to find a woman I don’t like and just give her a house.",
-	by: "Rod Stewart",
-	category: "comedy"
+	source: "Rod Stewart",
+	category: "comedy",
+	image: "images/rod_stewart.jpg"
 },
 {
 	quote: "Everything should be made as simple as possible, but not simpler.",
-	by: "Albert Einstein",
-	category: "wisdom"
+	source: "Albert Einstein",
+	category: "wisdom",
+	image: "images/albert_einstein.jpg"
 },
 {
 	quote: "We tend to overestimate what we can do in the short term and underestimate what we can do in the long term.",
-	by: "Tim Hurson",
-	category: "wisdom"
+	source: "Tim Hurson",
+	category: "wisdom",
+	image: "images/tim_hurson.jpg"
 },
 {
 	quote: "Knowledge is knowing a tomato is a fruit; wisdom is not putting it in a fruit salad.",
-	by: " Miles Kington",
-	category: "wisdom"
+	source: "Miles Kington",
+	category: "wisdom",
+	image: "images/miles_kington.jpg"
 },
 {
 	quote: "Quality means doing it right when no one is looking.",
-	by: "Henry Ford",
-	category: "wisdom"
+	source: "Henry Ford",
+	category: "wisdom",
+	image: "images/henry_ford.jpg"
 },
 {
 	quote: "The best way out is always through.",
-	by: "Robert Frost",
-	category: "wisdom"
+	source: "Robert Frost",
+	category: "wisdom",
+	image: "images/robert_frost.jpg"
 },
 {
 	quote: "The only thing we have to fear is fear itself.",
-	by: "Franklin D. Roosevelt",
-	category: "presidents"
+	source: "Franklin D. Roosevelt",
+	category: "presidents",
+	image: "images/franklin_d_roosevelt.jpg"
 },
 {
 	quote: "Education is the most powerful weapon which you can use to change the world.",
-	by: "Nelson Mandela",
-	category: "presidents"
+	source: "Nelson Mandela",
+	category: "presidents",
+	image: "images/nelson_mandela.jpg"
 },
 {
 	quote: "I am a slow walker, but I never walk back.",
-	by: "Abraham Lincoln",
-	category: "presidents"
+	source: "Abraham Lincoln",
+	category: "presidents",
+	image: "images/abraham_lincoln.jpg"
 },
 {
 	quote: "Associate with men of good quality if you esteem your own reputation; for it is better to be alone than in bad company.",
-	by: " George Washington",
-	category: "presidents"
+	source: "George Washington",
+	category: "presidents",
+	image: "images/george_washington.jpg"
 },
 {
 	quote: "The only man who never makes a mistake is the man who never does anything.",
-	by: "Theodore Roosevelt",
-	category: "presidents"
-};
+	source: "Theodore Roosevelt",
+	category: "presidents",
+	image: "images/theodore_roosevelt.jpg"
+}
+];
--- js/script.js
+++ js/script.js
@@ -1,4 +1,115 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+/*
+What I have to do:
+Create an array of JavaScript objects to hold the data for your quotes. Name array quotes. The quotes array should be accessible in the global scope.
+Each quote object should have the following properties:
+A quote property which contains a string: the text of the quote to display on the page
+A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb"
+An optional citation property which contains a string identifying the publication the quote appears in. For example, "Famous Anonymous Jokes". 
+If there is no known publication, then do not include this property on the object.
+An optional year property which contains a number identifying the date of the quote. For example, 1997. If there is no known date, then do not include this property on the object.
+Create a function named getRandomQuote which:
+selects a random quote object from the quotes array
+returns the randomly selected quote object
+Create a function named printQuote which follows these rules:
+printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+printQuote constructs a string using the different properties of the quote object using the following HTML template:
+ <p class="quote"> [quote here] </p> <p class="source"> [source here] <span class="citation"> [citation here] </span> <span class="year"> [year here] </span> </p>
+printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
+printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
+
+-------------
+Make sure you add code comments to document how your functions work
+Use JSHint (see the link in the Project Resources section) to analyze your JavaScript and identify any errors or code-style problems.
+Make sure your program is free of syntax errors. You can monitor any errors by looking at the Developer Tools console in your browser.
+Before you submit your project for review, make sure you can check off all of the items on the Student Project Submission Checklist.
+The checklist is designed to help you make sure you’ve met the grading requirements and that your project is complete and ready to be submitted!
+
+-----------------------
+BONUS: http://treehouse-techdegree.s3.amazonaws.com/Student-Project-Submission-Checklist.pdf
+--------------
+
+Add more properties to the quote object. For example, a tags property could include a list of "tags" like -- "humor", "business", "politics" -- to categorize each quote.
+Randomly change the background color of the page, when the quote changes
+Don't display a random quote more than once until ALL quotes from the array have been displayed.
+Refresh the quote after a set amount of time. For example, every 30 seconds, make a new quote appear. 
+(You can use the setInterval() or setTimeout() method to do this -- see the links in the Project Resources listing.)
+
+-------------------
+
+NOTE: A good practice is to check your project for cross browser compatibility.
+Making sure that it looks and functions correctly in multiple (at least three) browsers is an important part of being a top-notch developer.
+If you want, leave a comment to the project reviewer about which browser(s) the project was checked to ensure they are seeing things as you have designed them.
+Some browser options:
+Google Chrome
+Mozilla Firefox
+Internet Explorer/Edge
+Safari
+
+*/
+// STARTING CODE HERE:
+var quote = "";
+var source = "";
+var citation = "";
+var image = "";
+
+//Print the quotation to the page
+function printQuote(quote) {
+  var x = document.getElementsByClassName("quote");
+  x[0].innerHTML = quote;
+}
+
+//prints the source to the page
+function printSource(source) {
+	var x = document.getElementsByClassName("source");
+  	x[0].innerHTML = source;
+}
+
+//prints the citation to the page
+function printCitation(citation) {
+	var x = document.getElementsByClassName("citation");
+	x[0].innerHTML = citation;
+}
+
+var getImagePosition = '';
+
+// Creates the source for the image
+function getImage(image) {
+ var div = document.getElementById("image")
+ div.src = image;
+}
+
+
+printQuote(quotations[0].quote);
+printSource(quotations[0].source);
+//myFunction(quotations[0].citation);
+getImage(quotations[0].image);
+
+
+function myFunction(banana) {
+    var x = document.getElementById("quote-box");
+    x.getElementsByClassName("citation")[0].innerHTML = banana;
+}
+myFunction(quotations[0].citation);
+
+/*
+function myFunction(citation) {
+    var x = document.getElementsByClassName("citation");
+    for (i = 0; i < x.length; i++) {
+        x[i].innerHTML = citation;
+    }
+}
+Change the background color of all elements with class="example":
+
+var x = document.getElementsByClassName("example");
+var i;
+for (i = 0; i < x.length; i++) {
+    x[i].style.backgroundColor = "red";
+}
+*/
+
 
