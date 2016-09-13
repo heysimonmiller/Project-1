393e8a56d148d3ac09ad082f63133883f6296cbe
simonsayswhaat <simonmill@gmail.com>
2016-09-13 18:53:58 +0200

    Fixed timer
    Everything is working.
    Need to fix CSS styling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,112 @@
+// Used in the printQuote(quote) function
+var randomNumber;
+
+// Creates an array for the new numbers to be stored in - in makeUniqueRandom() function
+var uniqueRandoms = [];
+
+// Creates a variable used for the clickButton() function. It also starts the timer so after 30seconds, the quoation will change
+var timer = setInterval(printQuote, 30000);
+
+/*
+This function creates a unique random number from 0 - 19 (quotations.length)
+It will not repeat until all numbers have come up
+*/
+function makeUniqueRandom() {
+    // Refill the array if needed
+    if (!uniqueRandoms.length) {
+        for (var i = 0; i < quotations.length; i++) {
+            uniqueRandoms.push(i);
+        }
+    }
+    var index = Math.floor(Math.random() * uniqueRandoms.length);
+    var val = uniqueRandoms[index];
+
+    // Remove that selected value from the array
+    uniqueRandoms.splice(index, 1);
+
+    return val;
+}
+
+// Generates the quotation and adds it in the HTML
+function getQuote(quote) {
+  var x = document.getElementsByClassName("quote")
+  x[0].innerHTML = quote;
+}
+
+// Generates the source and adds it in the HTML
+function getSource(source) {
+	var x = document.getElementsByClassName("source");
+	x[0].innerHTML = source;
+}
+
+// Generates the citation and adds it in the HTML
+function getCitation(citation) {
+  var x = document.getElementsByClassName("citation")
+  x[0].innerHTML = citation;
+}
+
+// Generates the year and adds it in the HTML
+function getYear(year) {
+  var x = document.getElementsByClassName("year")
+  x[0].innerHTML = year;
+}
+
+// Creates the src for the image and adds it in the HTML
+function getImage(image) {
+ var div = document.getElementById("image");
+ div.src = image; //STILL NEED AN ALT IMAGE http://jsfiddle.net/Bc6Et/
+}
+
+// Generates a random color to use for changeBackgroundColor() to change the background color
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Changes the background color to a random color
+function changeBackgroundColor() {
+	document.body.style.backgroundColor = getRandomColor();
+}
+
+// Loads new data when the user presses the 'Show another quote' button
+function printQuote(quote) {
+	// Generates a new random number each time the function is called
+	randomNumber = makeUniqueRandom()
+	
+	// Inputs the HTML code and prints it to the page
+	getQuote(quotations[randomNumber].quote);
+	getSource(quotations[randomNumber].source);
+	getCitation(quotations[randomNumber].citation);
+	getYear(quotations[randomNumber].year);
+	getImage(quotations[randomNumber].image);
+
+	// Changes the background color to a random color
+	changeBackgroundColor();
+	console.log(randomNumber);
+}
+
+// This function resets the timer when the 'Show another quote' button is pressed.
+function clickButton(){ 
+   //If a timer is true, clear it's interval
+    if(timer) {
+        clearInterval(timer);
+    } 
+
+    printQuote();
+    // Start a new timer and store it in the timer variable stated at the top of the script
+    timer = setInterval(printQuote, 30000); 
+}
+
+// When the button 'Show another quote' is pressed, the clickButton() function runs.
+document.getElementById('loadQuote').addEventListener("click", clickButton, false); 
+
+// We run printQuote() once so that the first page load has a quotation, thereafter pressing the button or waiting 30s (timer) will load the next quotation.
+printQuote();
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 // document.getElementById('loadQuote').addEventListener("click", printQuote, false);
@@ -52,122 +161,3 @@ Safari
 
 */
 // STARTING CODE HERE:
-
-/*
-This function creates a unique random number from 0 - 19 (quotations.length)
-It will not repeat until all numbers have come up
-*/
-var uniqueRandoms = [];
-var numRandoms = quotations.length;
-function makeUniqueRandom() {
-    // refill the array if needed
-    if (!uniqueRandoms.length) {
-        for (var i = 0; i < numRandoms; i++) {
-            uniqueRandoms.push(i);
-        }
-    }
-    var index = Math.floor(Math.random() * uniqueRandoms.length);
-    var val = uniqueRandoms[index];
-
-    // now remove that value from the array
-    uniqueRandoms.splice(index, 1);
-
-    return val;
-
-}
-
-//Print the quotation to the page (adds in the html)
-function printQuote(quote) {
-  var x = document.getElementsByClassName("quote")
-  x[0].innerHTML = quote;
-}
-
-//prints the source to the page (adds in the html)
-function printSource(source) {
-	var x = document.getElementsByClassName("source");
-	x[0].innerHTML = source;
-}
-
-//Print the citation to the page (adds in the html)
-function printCitation(citation) {
-  var x = document.getElementsByClassName("citation")
-  x[0].innerHTML = citation;
-}
-
-//Print the year to the page (adds in the html)
-function printYear(year) {
-  var x = document.getElementsByClassName("year")
-  x[0].innerHTML = year;
-}
-
-// Creates the src for the image (adds in the html)
-function printImage(image) {
- var div = document.getElementById("image");
- div.src = image; //STILL NEED AN ALT IMAGE http://jsfiddle.net/Bc6Et/
-}
-
-// Generates a random color to use for changeBackgroundColor() to change the background color
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-// Changes the background color
-function changeBackgroundColor() {
-	document.body.style.backgroundColor = getRandomColor();
-}
-
-
-
-// Loads new data when the user presses the 'Show another quote' button
-function loadQuote(quote) {
-	// Generates a new random number each time the function is called
-	var randomNumber = makeUniqueRandom()
-	// Inputs the HTML code and prints it to the page
-	printQuote(quotations[randomNumber].quote);
-	printSource(quotations[randomNumber].source);
-	printCitation(quotations[randomNumber].citation);
-	printYear(quotations[randomNumber].year);
-	printImage(quotations[randomNumber].image);
-	changeBackgroundColor();
-	console.log(randomNumber);
-	clearInterval(timer);
-}
-
-
-var timer = setInterval(loadQuote(),10000);
-/*
-testing:
-
-
-function myFn() {console.log('idle');}
-
-var myTimer = setInterval(myFn, 4000);
-
-// Then, later at some future time, 
-// to restart a new 4 second interval starting at this exact moment in time
-clearInterval(myTimer);
-myTimer = setInterval(myFn, 4000);
-
-
-do 
-{	
-	var randomNumber = makeUniqueRandom();
-
-	//Inputs the HTML code and prints it to the page
-	printQuote(quotations[randomNumber].quote);
-	printSource(quotations[randomNumber].source);
-	printCitation(quotations[randomNumber].citation);
-	printYear(quotations[randomNumber].year);
-	printImage(quotations[randomNumber].image);
-	console.log(randomNumber);
-	//First time loading the page generates a random background color
-	changeBackgroundColor();
-}
-
-while (false)
-
-*/
